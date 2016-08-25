6b88bdc21eb23262e4b09202949a341a58bd699b
Jeff Eng <jeff@jeffeng.me>
2016-08-25 12:35:22 -0700

    set up initial project with project files, and deleted the template code

---
+++ Project20/Project20/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by Jeffrey Eng on 8/25/16.
+//  Copyright (c) 2016 Jeffrey Eng. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
