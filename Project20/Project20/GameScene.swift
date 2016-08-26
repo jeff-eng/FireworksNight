35efeec163b30da071c894f7274b5c7a7ad82098
Jeff Eng <jeff@jeffeng.me>
2016-08-26 11:28:35 -0700

    added global properties for game

--- Project20/Project20/GameScene.swift
+++ Project20/Project20/GameScene.swift
@@ -9,6 +9,22 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var gameTimer: NSTimer!
+    var fireworks: [SKNode]()
+    
+    // constants defining where we launch fireworks from
+    let leftEdge = -22
+    let bottomEdge = -22
+    let rightEdge = 1024 + 22
+    
+    // track player's score and update with a property observer
+    var score: Int = 0 {
+        didSet {
+            "Score: \(score)"
+        }
+    }
+    
     override func didMoveToView(view: SKView) {
 
     }
