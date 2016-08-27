0a4b3234cc645544768d3cb45b8ca19fd0c668ce
Jeff Eng <jeff@jeffeng.me>
2016-08-27 15:46:19 -0700

    added background to the game scene as well as repeating timer that calls launchFireworks method

--- Project20b/Project20b/GameScene.swift
+++ Project20b/Project20b/GameScene.swift
@@ -25,7 +25,18 @@ class GameScene: SKScene {
     }
     
     override func didMoveToView(view: SKView) {
-
+        // create SKSpriteNode instance
+        let background = SKSpriteNode(imageNamed: "background")
+        // set the sprite node's position
+        background.position = CGPoint(x: 512, y: 384)
+        // define the blend mode as Replace
+        background.blendMode = .Replace
+        // set the z-position of the background to -1
+        background.zPosition = -1
+        // add the node to the scene
+        addChild(background)
+        
+        gameTimer = NSTimer.scheduledTimerWithTimeInterval(6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -34,5 +45,9 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    func launchFireworks() {
+        
     }
 }
